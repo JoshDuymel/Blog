d3312c7f195b006fd36a22d01f4a409e8db6a5fe
Josh Duymel <joshduymel@gmail.com>
2019-10-10 13:46:22 +1300

    removed redundant code

--- numberGuesser.js
+++ numberGuesser.js
@@ -11,11 +11,10 @@ function writeMessage(elementId, message, appendMessage) {
 };
 
 function newGame() {
-  document.getElementById('resetButton');
 	secretNumber = Math.floor(Math.random() * 100) + 1;
 	numberOfGuesses = 0;
 	writeMessage('historyList', '');
-  writeMessage('statusArea', '<h2>Please enter a number 1-100 and press the Guess button.</h2>');
+  writeMessage('statusArea', '<p>Please enter a number 1-100 and press the Guess button.</p>');
 }
 
 function guessInRange(guess) {
@@ -24,50 +23,48 @@ function guessInRange(guess) {
 
 function userGuessed() {
 	var userGuessed = document.getElementById('userGuess').value;
-	var statusArea = document.getElementById('statusArea');
-	var historyList = document.getElementById('historyList');
 	if (userGuessed.length == 0 || ! guessInRange(userGuessed)) {
 
-		writeMessage('statusArea', '<h2>Please enter a number 1-100 and press the Guess button.</h2>');
+		writeMessage('statusArea', '<p>Please enter a number 1-100 and press the Guess button.</p>');
 	} else if (userGuessed.indexOf('.') != -1) {
-		writeMessage('statusArea', '<h2>Please enter a whole number 1-100 and press the Guess button.</h2>');
+		writeMessage('statusArea', '<p>Please enter a whole number 1-100 and press the Guess button.</p>');
 	} else {
 		numberOfGuesses++;
 
     if ((userGuessed < (secretNumber-50)) || (userGuessed > (secretNumber+50))) {
-			writeMessage('statusArea', '<h2>You\'re frozen! Your guess of ' + userGuessed + ' was 50 away! Try again...</h2>');
-			writeMessage('historyList', '<li>' + userGuessed +' (frozen,that was guess number ' + numberOfGuesses +')</li>', true);
+			writeMessage('statusArea', '<p>You\'re frozen! Your guess of ' + userGuessed + ' was more than 50 away! Try again...</p>');
+			writeMessage('historyList', '<li>' + userGuessed +'...frozen</li>', true);
 		}
     else if ((userGuessed < (secretNumber-40)) || (userGuessed > (secretNumber+40))) {
-			writeMessage('statusArea', '<h2>You\'re freezing! Your guess of ' + userGuessed + ' was 40 away! Try again...</h2>');
-			writeMessage('historyList', '<li>' + userGuessed +' (Freezing! That was guess number ' + numberOfGuesses +')</li>', true);
+			writeMessage('statusArea', '<p>You\'re freezing! Your guess of ' + userGuessed + ' was more than 40 away! Try again...</p>');
+			writeMessage('historyList', '<li>' + userGuessed +'...freezing</li>', true);
 		}
     else if ((userGuessed < (secretNumber-30)) || (userGuessed > (secretNumber+30))) {
-			writeMessage('statusArea', '<h2>You\'re cold! Your guess of ' + userGuessed + ' was 30 away! Try again...</h2>');
-			writeMessage('historyList', '<li>' + userGuessed +' (Cold! That was guess number ' + numberOfGuesses +')</li>', true);
+			writeMessage('statusArea', '<p>You\'re cold! Your guess of ' + userGuessed + ' was more than 30 away! Try again...</p>');
+			writeMessage('historyList', '<li>' + userGuessed +'...cold</li>', true);
 		}
     else if ((userGuessed < (secretNumber-20)) || (userGuessed > (secretNumber+20))) {
-			writeMessage('statusArea', '<h2>You\'re chilly! Your guess of ' + userGuessed + ' was 20 away! Try again...</h2>');
-			writeMessage('historyList', '<li>' + userGuessed +' (Chilly, that was guess number ' + numberOfGuesses +')</li>', true);
+			writeMessage('statusArea', '<p>You\'re chilly! Your guess of ' + userGuessed + ' was more than 20 away! Try again...</p>');
+			writeMessage('historyList', '<li>' + userGuessed +' chilly!</li>', true);
 		}
     else if ((userGuessed < (secretNumber-10)) || (userGuessed > (secretNumber+10))) {
-			writeMessage('statusArea', '<h2>You\'re warm! Your guess of ' + userGuessed + ' was 10 away! Try again...</h2>');
-			writeMessage('historyList', '<li>' + userGuessed +' (Warm...that was guess number ' + numberOfGuesses +')</li>', true);
+			writeMessage('statusArea', '<p>You\'re warm! Your guess of ' + userGuessed + ' was more than 10 away! Try again...</p>');
+			writeMessage('historyList', '<li>' + userGuessed +' warm...</li>', true);
 		}
     else if ((userGuessed < (secretNumber-5)) || (userGuessed > (secretNumber+5))) {
-			writeMessage('statusArea', '<h2>You\'re hot! ;) Your guess of ' + userGuessed + ' was 5 away! Try again...</h2>');
-			writeMessage('historyList', '<li>' + userGuessed +' (Hot! That was guess number ' + numberOfGuesses +')</li>', true);
+			writeMessage('statusArea', '<p>You\'re hot! ;) Your guess of ' + userGuessed + ' was more than 5 away! Try again...</p>');
+			writeMessage('historyList', '<li>' + userGuessed +' hot!</li>', true);
 		}
     else if ((userGuessed < (secretNumber-2)) || (userGuessed > (secretNumber+2))) {
-			writeMessage('statusArea', '<h2>You\'re roastin\'! ;D Your guess of ' + userGuessed + ' was 2 away! Try again...</h2>');
-			writeMessage('historyList', '<li>' + userGuessed +' (Roastin! That was guess number ' + numberOfGuesses +')</li>', true);
+			writeMessage('statusArea', '<p>You\'re roastin\'! ;D Your guess of ' + userGuessed + ' was more than 2 away! Try again...</p>');
+			writeMessage('historyList', '<li>' + userGuessed +' you\'re roatin\'!</li>', true);
 		}
     else if ((userGuessed <= (secretNumber-1)) || (userGuessed >= (secretNumber+1))) {
-			writeMessage('statusArea', '<h2>You\'re on fire! :O Your guess of ' + userGuessed + ' was 1 away! Try again...</h2>');
-			writeMessage('historyList', '<li>' + userGuessed +' (Burning! That was guess number ' + numberOfGuesses +')</li>', true);
+			writeMessage('statusArea', '<p>You\'re on fire! :O Your guess of ' + userGuessed + ' was more than 1 away! Try again...</p>');
+			writeMessage('historyList', '<li>' + userGuessed +' wew, you\'re on fire!</li>', true);
 		}
     else if (userGuessed == secretNumber) {
-			writeMessage('statusArea', '<h2>You got me in ' + numberOfGuesses +' guesses! It was ' + secretNumber + '! Let\'s try again!</h2>');
+			writeMessage('statusArea', '<p>You got me in ' + numberOfGuesses +' guesses! It was ' + secretNumber + '! Let\'s try again!</p>');
       	writeMessage('historyList', '<li>Ah damn! Ya\' got me :D</li>', true);
 		}
 	}
